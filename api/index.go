996318976fac1859d955faaea7c1ad8c23e325dd
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-01-06 15:00:28 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,35 +1,24 @@
 package handler
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
-
-	gogpt "github.com/sashabaranov/go-gpt3"
+	
 	"github.com/yanzay/tbot/v2"
 )
 
 // HandlerFunc is the signature type for the main function that will handle HTTP requests.
 func HandlerFunc(w http.ResponseWriter, r *http.Request) {
-	// Create a new instance of the GPT-3 client.
-	c := gogpt.NewClient(os.Getenv("OPENAI_TOKEN"))
-	ctx := context.Background()
-
-	bot := tbot.New(os.Getenv("TELEGRAM_BOT_TOKEN"),
-		tbot.WithWebhook("https://golang-chat-gpt-telegram-bot-vercel.vercel.app/", ":8080"))
-	c1 := bot.Client() //Please add your Render URL between "". 請在引號中加入你的Render網址
-
-	/////////////////
-
-	bot.HandleMessage("ping", func(m *tbot.Message) {
-			c1.SendMessage(m.Chat.ID, "pong")
+bot := tbot.New(os.Getenv("TELEGRAM_BOT_TOKEN"),
+			tbot.WithWebhook("https://golang-chat-gpt-telegram-bot-vercel.vercel.app/", ":8080"))
+		c := bot.Client()
+		bot.HandleMessage("ping", func(m *tbot.Message) {
+			c.SendMessage(m.Chat.ID, "pong")
 		})
-	log.Fatal(bot.Start())
+		log.Fatal(bot.Start())
 }
 
-func Main() {
+func main() {
 http.HandleFunc("/", HandlerFunc)
 http.ListenAndServe(":8080", nil)
 }
